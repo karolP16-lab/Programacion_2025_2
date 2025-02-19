98fd380075e5c5192fc42f6bdb5c6374a167314c
karolP16-lab <karolveronicap16b29@gmail.com>
2025-02-19 01:56:31 -0600

    Add files via upload

---
+++ Cuenta.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 19:23:00 2025
+
+@author: Tlahuizcalpan
+"""
+#class Cuenta
+def  init  (self,valor,tipo,nombre):
+    self.saldo=valor
+    self.tipo=tipo
+    self.nombre=nombre 
+def imptimitdetalles (self):    
+    print("desde el metodo")
+    print("saldo=",self.saldo)
+    print("tipo=",self.tipo)
+    print("nombre=",self.nombre)
